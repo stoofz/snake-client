346167d82ec850d48184f54552c0afd7cec6c98f
Anthony Chambers <ajfchambers@gmail.com>
2023-03-14 10:50:07 -0400

    input switch added, lines cleaned up play.js

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,8 +11,22 @@ const setupInput = function () {
 }
 
 const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
+  switch (key) {
+    case '\u0003':
+      console.log("Quitting game")
+      process.exit();
+    case 'w':
+      connection.write("Move: up");
+      break;
+    case 'a':
+      connection.write("Move: left");
+       break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'd':
+      connection.write("Move: right");
+      break;
   }
 };
 
