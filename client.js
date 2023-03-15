9452e9329f05cf4fc207bd10d30af39b0e4637a5
Anthony Chambers <ajfchambers@gmail.com>
2023-03-14 22:51:43 -0400

    added process.exit() for connection ended

--- client.js
+++ client.js
@@ -1,9 +1,16 @@
 const net = require("net");
-
 const { IP, PORT } = require('./constants');
 
+// Takes initials, if not assigns P1 as default
+const initials = function() {
+  if (process.argv.slice(2).length === 0) {
+    return 'P1';
+  }
+  return process.argv.slice(2);
+};
 
-const connect = function () {
+// Connect to server
+const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT,
@@ -13,12 +20,17 @@ const connect = function () {
 
   conn.on("connect", () => {
     console.log('Connected to server!');
-    conn.write('Name: AJC')
+    conn.write(`Name: ${initials()}`);
+  });
+
+  conn.on('end', () => {
+    console.log('Connection ended');
+    process.exit();
   });
 
   conn.on("data", (data) => {
     console.log("Server message: ", data);
-    });
+  });
   
   return conn;
 };
