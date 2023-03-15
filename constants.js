cd22513af8a72fa3e53a407cdb76243276b985c8
Anthony Chambers <ajfchambers@gmail.com>
2023-03-14 22:49:46 -0400

    constants added, msg object

--- constants.js
+++ constants.js
@@ -1,7 +1,22 @@
 const IP = 'localhost';
 const PORT = 50541;
+const MOVE_DOWN_KEY = 's';
+const MOVE_UP_KEY = 'w';
+const MOVE_RIGHT_KEY = 'd';
+const MOVE_LEFT_KEY = 'a';
+const MSG = {
+  'b': 'Say: Snakes be snaking',
+  'n': 'Say: Hissssssssssssss',
+  'm': 'Say: Keep on slithering',
+  'v': 'Say: ssSssSnaaaaakeeee',
+};
 
 module.exports = {
   IP,
   PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MSG,
 };
